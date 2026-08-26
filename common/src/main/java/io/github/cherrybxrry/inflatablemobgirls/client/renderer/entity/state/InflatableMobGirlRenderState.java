3e4631e61806c0c6fc51092d31adfaeadcfa8091
Cherry <kieran.kc.mc@gmail.com>
2026-08-26 09:57:03 +0200

    Move stage variable to avoid repeats

--- common/src/main/java/io/github/cherrybxrry/inflatablemobgirls/client/renderer/entity/state/InflatableMobGirlRenderState.java
+++ common/src/main/java/io/github/cherrybxrry/inflatablemobgirls/client/renderer/entity/state/InflatableMobGirlRenderState.java
@@ -3,5 +3,6 @@ package io.github.cherrybxrry.inflatablemobgirls.client.renderer.entity.state;
 import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
 
 public abstract class InflatableMobGirlRenderState extends LivingEntityRenderState {
+    public int stage = 0;
     public boolean isSpawner;
 }
